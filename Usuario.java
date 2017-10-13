e25ddb472f39e19ccc75fb83cfcd28ebe63720f6
val17102 <32184574+val17102@users.noreply.github.com>
2017-10-12 19:28:18 -0600

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario
+{
+	private String Codigo;
+	private String Name;
+  
+  public Usuario(String Codigo, String Name){
+  
+  this.Codigo = Codigo;
+  this.Name = Name;
+  }
+  
+  }
